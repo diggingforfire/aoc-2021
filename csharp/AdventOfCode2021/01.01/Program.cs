c9b9912af01bfd871502f55c2dbd56f9e8228da4
Robbie Luijben <diggingforfire@gmail.com>
2021-12-01 07:14:13 +0100

    Day 1

---
+++ csharp/AdventOfCode2021/01.01/Program.cs
@@ -0,0 +1,3 @@
+﻿var input = (await File.ReadAllLinesAsync("input.txt")).Select(int.Parse).ToArray();
+var result = input.Skip(1).Select((cur, i) => new { cur, prev = input[i] }).Count(x => x.cur > x.prev);
+Console.WriteLine(result);
---
+++ csharp/AdventOfCode2021/01.02/Program.cs
@@ -0,0 +1,4 @@
+﻿var input = (await File.ReadAllLinesAsync("input.txt")).Select(int.Parse).ToArray();
+var sums = input.Skip(2).Select((cur, i) => cur + input[i] + input[i + 1]).ToArray();
+var result = sums.Skip(1).Select((cur, i) => new { cur, prev = sums[i] }).Count(x => x.cur > x.prev);
+Console.WriteLine(result);
