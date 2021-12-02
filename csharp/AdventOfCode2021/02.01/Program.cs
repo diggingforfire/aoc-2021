2fd45f63db160a3ee4b369bfbd4cab4925fad225
Robbie Luijben <diggingforfire@gmail.com>
2021-12-02 06:17:31 +0100

    Day 2

---
+++ csharp/AdventOfCode2021/02.01/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = (await File.ReadAllLinesAsync("input.txt"))
+    .Select(line => line.Split(" "))
+    .Select(parts => new
+    {
+        Command = parts[0],
+        X = int.Parse(parts[1])
+    });
+
+int depth = 0;
+int pos = 0;
+
+foreach (var cmd in input)
+{
+    switch (cmd.Command)
+    {
+        case "down": depth += cmd.X; break;
+        case "up": depth -= cmd.X; break;
+        case "forward": pos += cmd.X; break;
+    }
+}
+
+var result = depth * pos;
+Console.WriteLine(result);
---
+++ csharp/AdventOfCode2021/02.02/Program.cs
@@ -0,0 +1,27 @@
+﻿var input = (await File.ReadAllLinesAsync("input.txt"))
+    .Select(line => line.Split(" "))
+    .Select(parts => new
+    {
+        Command = parts[0],
+        X = int.Parse(parts[1])
+    });
+
+int depth = 0;
+int pos = 0;
+int aim = 0;
+
+foreach (var cmd in input)
+{
+    switch (cmd.Command)
+    {
+        case "down": aim += cmd.X; break;
+        case "up": aim -= cmd.X; break;
+        case "forward": 
+            pos += cmd.X; 
+            depth += aim * cmd.X; 
+            break;
+    }
+}
+
+var result = depth * pos;
+Console.WriteLine(result);
