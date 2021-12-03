24b652a7205a75569453fe85693925e2b09e7ecd
Robbie Luijben <diggingforfire@gmail.com>
2021-12-03 08:21:48 +0100

    Day 3 part 1

---
+++ csharp/AdventOfCode2021/03.01/Program.cs
@@ -0,0 +1,23 @@
+﻿var lines = (await File.ReadAllLinesAsync("input.txt"));
+
+var gammas = lines
+    .SelectMany(line => line) // all chars
+    .Select((c, i) => new { Char = c, Index = i }) // all chars with their index
+    .GroupBy(p => p.Index % lines[0].Length) // grouped by their position
+    .Select(grp =>
+    new
+    {
+        Gamma = grp
+            .GroupBy(charWithIndex => charWithIndex.Char) // grouped by char
+            .OrderByDescending(charGroup => charGroup.Count()).ToList()
+    })
+    .Select(gammas => new 
+    { 
+        First = gammas.Gamma[0].Key, 
+        Second = gammas.Gamma[1].Key 
+    });
+
+var gamma = Convert.ToInt32(new string(gammas.Select(x => x.First).ToArray()), 2);
+var epsilon = Convert.ToInt32(new string(gammas.Select(x => x.Second).ToArray()), 2);
+
+Console.WriteLine(gamma * epsilon);
